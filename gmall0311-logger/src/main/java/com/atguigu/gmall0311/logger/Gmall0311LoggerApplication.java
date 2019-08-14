b0342aa3c6bf7b9217783452f8c992fd9bddd585
baishaowei <1040866384@qq.com>
2019-08-14 20:10:24 +0800

    一点小改动

--- gmall0311-logger/src/main/java/com/atguigu/gmall0311/logger/Gmall0311LoggerApplication.java
+++ gmall0311-logger/src/main/java/com/atguigu/gmall0311/logger/Gmall0311LoggerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Gmall0311LoggerApplication {
-
+    //kkk
     public static void main(String[] args) {
         SpringApplication.run(Gmall0311LoggerApplication.class, args);
     }
